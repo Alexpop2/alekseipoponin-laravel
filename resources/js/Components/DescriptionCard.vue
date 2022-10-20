05f68b560ba9ce14016065050c8432139b12ad7f
Aleksei Poponin <alexqw1@ya.ru>
2022-10-20 03:28:45 +0300

    Scroll fix

--- resources/js/Components/DescriptionCard.vue
+++ resources/js/Components/DescriptionCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full lg:h-full mt-4 lg:mt-0 lg:absolute lg:pl-[500px] flex" v-motion-fade>
-        <div class="border lg:rounded-r-lg bg-white h-[90%] w-full my-auto p-8">
+        <div class="border lg:rounded-r-lg bg-white h-[90%] w-full my-auto p-8 lg:overflow-y-scroll">
             <slot />
         </div>
     </div>
