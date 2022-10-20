d5d8b6a0f6344b355294aea0e503b8050c5b08ae
Aleksei Poponin <alexqw1@ya.ru>
2022-10-20 03:14:47 +0300

    Service component done

---
+++ resources/js/Components/Service.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <div class="text-2xl w-[50px] h-[50px] flex mx-auto rounded-full bg-gradient-to-br from-gray-200"><span class="m-auto"><slot name="icon" /></span></div>
+        <slot name="text" />
+    </div>
+</template>
+
--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -4,6 +4,7 @@ import TextHeader from "../Components/TextHeader.vue";
 import Container from "../Components/Container.vue";
 import PersonCard from "../Components/PersonCard.vue";
 import DescriptionCard from "../Components/DescriptionCard.vue";
+import Service from "../Components/Service.vue";
 </script>
 
 <template>
@@ -12,10 +13,25 @@ import DescriptionCard from "../Components/DescriptionCard.vue";
             <PersonCard />
             <DescriptionCard>
                 <TextHeader>
-                    About me
+                    About Me
                 </TextHeader>
                 <div class="font-light">
                     Hello. My name is Aleksei Poponin. I'm Full stack web developer with 8+ years experience. Mainly working with PHP, CSS, JavaScript, Laravel and Vue.js.
+                </div>
+                <TextHeader class="mt-12">
+                    My Services
+                </TextHeader>
+                <div class="font-light">
+                    <div class="grid grid-cols-2 gap-4 text-center">
+                        <Service>
+                            <template #icon><i class="fa-brands fa-html5"></i></template>
+                            <template #text>Frontend</template>
+                        </Service>
+                        <Service>
+                            <template #icon><i class="fa-solid fa-code"></i></template>
+                            <template #text>Backend</template>
+                        </Service>
+                    </div>
                 </div>
             </DescriptionCard>
         </Container>
