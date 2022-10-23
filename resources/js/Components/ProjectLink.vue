43f3017bc3d5f69a7bed5c7ee8420d945f0f141d
Aleksei Poponin <alexqw1@ya.ru>
2022-10-23 21:36:41 +0300

    Projects done

---
+++ resources/js/Components/ProjectLink.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {defineProps} from "vue";
+
+defineProps({
+   href: String,
+   img: String,
+});
+</script>
+
+<template>
+    <div class="relative opacity-25 hover:opacity-100 transition-all border border-gray-500 rounded">
+        <a :href="href"><div class="w-full h-[200px] bg-center bg-contain bg-no-repeat cursor-pointer transition-all" :style="`background-image: url('${img}')`"></div></a>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Components/Projects.vue
+++ resources/js/Components/Projects.vue
@@ -1,7 +1,8 @@
 <script setup>
 import TextHeader from "../Components/TextHeader.vue";
 import DescriptionCard from "../Components/DescriptionCard.vue";
-import Service from "../Components/Service.vue";
+import ProjectLink from "./ProjectLink.vue";
+
 </script>
 
 <template>
@@ -10,7 +11,60 @@ import Service from "../Components/Service.vue";
             Projects
         </TextHeader>
         <div class="font-light">
-            Projects
+            <h3 class="text-xl">IMEMO</h3>
+            <div>imemo.ru whole website development. Many projects have been done with pure PHP, JavaScript, CSS, Bootstrap. Many projects have been done with Laravel + Vue.js frameworks. For public and private usage.</div>
+            <div>Most valuable: </div>
+            <ul class="list-disc list-inside">
+                <li>Online application system for postgraduate school.</li>
+                <li>Online voting system in the competitions for vacancies.</li>
+                <li>Online voting system for academic council tasks.</li>
+                <li>REB Statistics graphs.</li>
+                <li>Energyeconomics section graphs.</li>
+            </ul>
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4 text-center mt-4">
+                <div>
+                    <ProjectLink
+                        href="https://t.ly/fN8m"
+                        img="/assets/imemo.png"
+                    />
+                </div>
+                <div>
+                    <ProjectLink
+                        href="https://t.ly/-PVY"
+                        img="/assets/reb.png"
+                    />
+                </div>
+            </div>
+            <h3 class="text-xl mt-4">afjournal.ru</h3>
+            <div>Analysis and Forecasting. IMEMO Journal. Website development.</div>
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4 text-center mt-4">
+                <div>
+                    <ProjectLink
+                        href="https://t.ly/sbr3"
+                        img="/assets/afjournal.png"
+                    />
+                </div>
+            </div>
+            <h3 class="text-xl mt-4">primakovreadings.com</h3>
+            <div>Primakov Readings meetings. Website development.</div>
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4 text-center mt-4">
+                <div>
+                    <ProjectLink
+                        href="https://t.ly/x4rc"
+                        img="/assets/primakovreadings.png"
+                    />
+                </div>
+            </div>
+            <h3 class="text-xl mt-4">Self projects</h3>
+            <div>Coopnotes - for share your notes with other users.</div>
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4 text-center mt-4">
+                <div>
+                    <ProjectLink
+                        href="https://t.ly/Mzsc"
+                        img="/assets/coopnotes.png"
+                    />
+                </div>
+            </div>
         </div>
     </DescriptionCard>
 </template>
